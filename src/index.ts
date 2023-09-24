337f90ef943cd9b6c5e7eca74719c0875c33b790
Yosuke Torii <jinjorweb@gmail.com>
2023-09-25 03:14:02 +0900

    getRows

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,79 @@
-export const foo = 42;
+type DepsMap = Map<string, string[]>;
+type Graph = {
+  map: DepsMap;
+  inverseMap: DepsMap;
+  relations: Set<string>;
+};
+
+const appendToMap = (map: DepsMap, key: string, value: string) => {
+  const values = map.get(key);
+  if (values) {
+    values.push(value);
+  } else {
+    map.set(key, [value]);
+  }
+};
+
+const cloneMap = (map: DepsMap): DepsMap => {
+  const newMap = new Map<string, string[]>();
+  for (const [key, value] of map) {
+    newMap.set(key, [...value]);
+  }
+  return newMap;
+};
+
+export const newGraph = (): Graph => {
+  return {
+    map: new Map<string, string[]>(),
+    inverseMap: new Map<string, string[]>(),
+    relations: new Set<string>(),
+  };
+};
+
+export const add = (graph: Graph, from: string, to: string): boolean => {
+  const relation = `${from} ${to}`;
+  if (graph.relations.has(relation)) {
+    return false;
+  }
+  appendToMap(graph.map, from, to);
+  appendToMap(graph.inverseMap, to, from);
+  graph.relations.add(relation);
+  return true;
+};
+
+export const getRows = (graph: Graph): string[] => {
+  const row: string[] = [];
+  const map = cloneMap(graph.map);
+  const inverseMap = cloneMap(graph.inverseMap);
+
+  const findFirstNode = () => {
+    return [...map.keys()].find((key) => !inverseMap.has(key));
+  };
+  const collect = (node: string) => {
+    row.push(node);
+    const nextNodes = map.get(node);
+    if (!nextNodes) {
+      return;
+    }
+    for (const nextNode of nextNodes) {
+      const inverseValues = inverseMap.get(nextNode)!;
+      inverseValues.splice(inverseValues.indexOf(node), 1);
+      if (inverseValues.length === 0) {
+        inverseMap.delete(nextNode);
+        collect(nextNode);
+      }
+    }
+    map.delete(node);
+  };
+  while (true) {
+    const node = findFirstNode();
+    if (!node) {
+      break;
+    }
+    collect(node);
+  }
+  if (map.size > 0 || inverseMap.size > 0) {
+    throw new Error("Circular dependency detected");
+  }
+  return row;
+};
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,48 @@
 import assert from "node:assert";
-import test from "node:test";
-import { foo } from "../dist";
+import { describe, test } from "node:test";
+import { newGraph, add, getRows } from "../dist";
 
-test("foo", () => {
-  assert.strictEqual(foo, 42);
+test("add", () => {
+  const graph = newGraph();
+  assert.strictEqual(add(graph, "a", "b"), true);
+  assert.strictEqual(add(graph, "a", "b"), false);
+});
+
+describe("getRows", () => {
+  test("empty", () => {
+    const graph = newGraph();
+    assert.deepStrictEqual(getRows(graph), []);
+  });
+  test("single", () => {
+    const graph = newGraph();
+    add(graph, "a", "b");
+    assert.deepStrictEqual(getRows(graph), ["a", "b"]);
+  });
+  test("multi inputs", () => {
+    const graph = newGraph();
+    add(graph, "a", "c");
+    add(graph, "b", "c");
+    assert.deepStrictEqual(getRows(graph), ["a", "b", "c"]);
+  });
+  test("multi outputs", () => {
+    const graph = newGraph();
+    add(graph, "a", "b");
+    add(graph, "a", "c");
+    assert.deepStrictEqual(getRows(graph), ["a", "b", "c"]);
+  });
+  test("triangle", () => {
+    const graph = newGraph();
+    add(graph, "b", "c");
+    add(graph, "a", "b");
+    add(graph, "a", "c");
+    assert.deepStrictEqual(getRows(graph), ["a", "b", "c"]);
+  });
+  test("diamond", () => {
+    const graph = newGraph();
+    add(graph, "b", "d");
+    add(graph, "c", "d");
+    add(graph, "a", "b");
+    add(graph, "a", "c");
+    assert.deepStrictEqual(getRows(graph), ["a", "b", "c", "d"]);
+  });
 });
