a8d32f0c4decce2d219a6ada5633400e19921c35
Yosuke Torii <jinjorweb@gmail.com>
2023-09-26 22:47:16 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
 type DepsMap = Map<string, string[]>;
-type Graph = {
-  map: DepsMap;
-  inverseMap: DepsMap;
-  relations: Set<string>;
-};
-
 const appendToMap = (map: DepsMap, key: string, value: string) => {
   const values = map.get(key);
   if (values) {
@@ -13,7 +7,6 @@ const appendToMap = (map: DepsMap, key: string, value: string) => {
     map.set(key, [value]);
   }
 };
-
 const cloneMap = (map: DepsMap): DepsMap => {
   const newMap = new Map<string, string[]>();
   for (const [key, value] of map) {
@@ -22,178 +15,170 @@ const cloneMap = (map: DepsMap): DepsMap => {
   return newMap;
 };
 
-export const create = (): Graph => {
-  return {
-    map: new Map<string, string[]>(),
-    inverseMap: new Map<string, string[]>(),
-    relations: new Set<string>(),
-  };
-};
-
-export const add = (graph: Graph, from: string, to: string): boolean => {
-  const relation = `${from} ${to}`;
-  if (graph.relations.has(relation)) {
-    return false;
+export class Graph {
+  private map: DepsMap = new Map();
+  private inverseMap: DepsMap = new Map();
+  private relations: Set<string> = new Set();
+  has(from: string, to: string): boolean {
+    return this.relations.has(`${from} ${to}`);
   }
-  appendToMap(graph.map, from, to);
-  appendToMap(graph.inverseMap, to, from);
-  graph.relations.add(relation);
-  return true;
-};
-
-export const from = (graph: Graph, ...from: string[]): Graph => {
-  const newGraph = create();
-  const collect = (node: string) => {
-    const nextNodes = graph.map.get(node);
-    if (!nextNodes) {
-      return;
+  add(from: string, to: string): boolean {
+    if (this.has(from, to)) {
+      return false;
     }
-    for (const nextNode of nextNodes) {
-      const added = add(newGraph, node, nextNode);
-      if (!added) {
-        continue;
+    appendToMap(this.map, from, to);
+    appendToMap(this.inverseMap, to, from);
+    this.relations.add(`${from} ${to}`);
+    return true;
+  }
+  from(...from: string[]): Graph {
+    const newGraph = new Graph();
+    const collect = (node: string) => {
+      const nextNodes = this.map.get(node);
+      if (!nextNodes) {
+        return;
       }
-      collect(nextNode);
-    }
-  };
-  from.forEach(collect);
-  return newGraph;
-};
-
-export const to = (graph: Graph, ...to: string[]): Graph => {
-  const newGraph = create();
-  const collect = (node: string) => {
-    const prevNodes = graph.inverseMap.get(node);
-    if (!prevNodes) {
-      return;
-    }
-    for (const prevNode of prevNodes) {
-      const added = add(newGraph, prevNode, node);
-      if (!added) {
-        continue;
-      }
-      collect(prevNode);
-    }
-  };
-  to.forEach(collect);
-  return newGraph;
-};
-
-export const getRows = (graph: Graph): string[] => {
-  const rows: string[] = [];
-  const map = cloneMap(graph.map);
-  const inverseMap = cloneMap(graph.inverseMap);
-
-  const findFirstNode = () => {
-    return [...map.keys()].find((key) => !inverseMap.has(key));
-  };
-  const collect = (node: string) => {
-    rows.push(node);
-    const nextNodes = map.get(node);
-    if (!nextNodes) {
-      return;
-    }
-    for (const nextNode of nextNodes) {
-      const inverseValues = inverseMap.get(nextNode)!;
-      inverseValues.splice(inverseValues.indexOf(node), 1);
-      if (inverseValues.length === 0) {
-        inverseMap.delete(nextNode);
+      for (const nextNode of nextNodes) {
+        const added = newGraph.add(node, nextNode);
+        if (!added) {
+          continue;
+        }
         collect(nextNode);
       }
-    }
-    map.delete(node);
-  };
-  while (true) {
-    const node = findFirstNode();
-    if (!node) {
-      break;
-    }
-    collect(node);
+    };
+    from.forEach(collect);
+    return newGraph;
   }
-  if (map.size > 0 || inverseMap.size > 0) {
-    throw new Error("Circular dependency detected");
-  }
-  return rows;
-};
-
-export const getCols = (graph: Graph): string[][] => {
-  const cols: string[][] = [];
-  const { map } = graph;
-  const inverseMap = cloneMap(graph.inverseMap);
-  const visited = new Set<string>();
-
-  const collect = (node: string, index = 0, prev?: string) => {
-    if (visited.has(node)) {
-      return;
-    }
-    const existing = cols[index];
-    if (existing) {
-      if (prev && existing.some((item) => map.get(item)?.includes(prev))) {
-        cols.splice(index, 0, [node]);
-      } else {
-        existing.push(node);
+  to(...to: string[]): Graph {
+    const newGraph = new Graph();
+    const collect = (node: string) => {
+      const prevNodes = this.inverseMap.get(node);
+      if (!prevNodes) {
+        return;
       }
-    } else {
-      cols.push([node]);
-    }
-    visited.add(node);
-
-    const nextNodes = inverseMap.get(node);
-    if (!nextNodes) {
-      return;
-    }
-    nextNodes.reverse();
-    for (const nextNode of nextNodes) {
-      collect(nextNode, index + 1, node);
-    }
-  };
-
-  const firstNodes = [...inverseMap.keys()].filter((key) => !map.has(key));
-  firstNodes.reverse();
-  for (const node of firstNodes) {
-    collect(node);
-  }
-  cols.reverse();
-  return cols;
-};
-
-export function* getLines(
-  graph: Graph,
-  rows: string[],
-  cols: string[][]
-): Generator<string> {
-  const rowContext = Array.from({ length: cols.length }).map(
-    () => new Set<string>()
-  );
-  for (const row of rows) {
-    let line = "";
-    let hit = false;
-    for (let i = 0; i < cols.length; i++) {
-      if (rowContext[i].has(row)) {
-        rowContext[i].delete(row);
-        if (rowContext[i].size === 0) {
-          line += hit ? "┴" : "└";
-        } else {
-          line += "├";
+      for (const prevNode of prevNodes) {
+        const added = newGraph.add(prevNode, node);
+        if (!added) {
+          continue;
         }
-        hit = true;
-      } else {
-        if (cols[i].includes(row)) {
-          line += "◊";
-          hit = false;
-          for (const toBeFound of graph.map.get(row) ?? []) {
-            rowContext[i].add(toBeFound);
-          }
+        collect(prevNode);
+      }
+    };
+    to.forEach(collect);
+    return newGraph;
+  }
+  getRows(): string[] {
+    const rows: string[] = [];
+    const map = cloneMap(this.map);
+    const inverseMap = cloneMap(this.inverseMap);
+
+    const findFirstNode = () => {
+      return [...map.keys()].find((key) => !inverseMap.has(key));
+    };
+    const collect = (node: string) => {
+      rows.push(node);
+      const nextNodes = map.get(node);
+      if (!nextNodes) {
+        return;
+      }
+      for (const nextNode of nextNodes) {
+        const inverseValues = inverseMap.get(nextNode)!;
+        inverseValues.splice(inverseValues.indexOf(node), 1);
+        if (inverseValues.length === 0) {
+          inverseMap.delete(nextNode);
+          collect(nextNode);
+        }
+      }
+      map.delete(node);
+    };
+    while (true) {
+      const node = findFirstNode();
+      if (!node) {
+        break;
+      }
+      collect(node);
+    }
+    if (map.size > 0 || inverseMap.size > 0) {
+      throw new Error("Circular dependency detected");
+    }
+    return rows;
+  }
+  getCols(): string[][] {
+    const cols: string[][] = [];
+    const { map } = this;
+    const inverseMap = cloneMap(this.inverseMap);
+    const visited = new Set<string>();
+
+    const collect = (node: string, index = 0, prev?: string) => {
+      if (visited.has(node)) {
+        return;
+      }
+      const existing = cols[index];
+      if (existing) {
+        if (prev && existing.some((item) => this.has(item, prev))) {
+          cols.splice(index, 0, [node]);
         } else {
-          if (rowContext[i].size > 0) {
-            line += "│";
+          existing.push(node);
+        }
+      } else {
+        cols.push([node]);
+      }
+      visited.add(node);
+
+      const nextNodes = inverseMap.get(node);
+      if (!nextNodes) {
+        return;
+      }
+      nextNodes.reverse();
+      for (const nextNode of nextNodes) {
+        collect(nextNode, index + 1, node);
+      }
+    };
+
+    const firstNodes = [...inverseMap.keys()].filter((key) => !map.has(key));
+    firstNodes.reverse();
+    for (const node of firstNodes) {
+      collect(node);
+    }
+    cols.reverse();
+    return cols;
+  }
+  *getLines(): Generator<string> {
+    const rows = this.getRows();
+    const cols = this.getCols();
+    const rowContext = Array.from({ length: cols.length }).map(
+      () => new Set<string>()
+    );
+    for (const row of rows) {
+      let line = "";
+      let hit = false;
+      for (let i = 0; i < cols.length; i++) {
+        if (rowContext[i].has(row)) {
+          rowContext[i].delete(row);
+          if (rowContext[i].size === 0) {
+            line += hit ? "┴" : "└";
           } else {
-            line += hit ? "─" : " ";
+            line += "├";
+          }
+          hit = true;
+        } else {
+          if (cols[i].includes(row)) {
+            line += "◊";
+            hit = false;
+            for (const toBeFound of this.map.get(row) ?? []) {
+              rowContext[i].add(toBeFound);
+            }
+          } else {
+            if (rowContext[i].size > 0) {
+              line += "│";
+            } else {
+              line += hit ? "─" : " ";
+            }
           }
         }
       }
+      line += " " + row;
+      yield line;
     }
-    line += " " + row;
-    yield line;
   }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,241 +1,240 @@
 import assert from "node:assert";
 import { describe, test } from "node:test";
-import { create, add, from, getRows } from "../dist";
-import { getCols, getLines, to } from "../src";
+import { Graph } from "../dist";
 
 describe("add", () => {
   test("add", () => {
-    const graph = create();
-    assert.strictEqual(add(graph, "a", "b"), true);
-    assert.strictEqual(add(graph, "a", "b"), false);
+    const graph = new Graph();
+    assert.strictEqual(graph.add("a", "b"), true);
+    assert.strictEqual(graph.add("a", "b"), false);
   });
 });
 
 describe("from", () => {
   test("1/2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    const newGraph = from(graph, "a");
-    assert.strictEqual(add(newGraph, "a", "b"), false);
+    const graph = new Graph();
+    graph.add("a", "b");
+    const newGraph = graph.from("a");
+    assert.strictEqual(newGraph.has("a", "b"), true);
   });
   test("2/2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    const newGraph = from(graph, "b");
-    assert.strictEqual(add(newGraph, "a", "b"), true);
+    const graph = new Graph();
+    graph.add("a", "b");
+    const newGraph = graph.from("b");
+    assert.strictEqual(newGraph.has("a", "b"), false);
   });
   test("?/2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    const newGraph = from(graph, "c");
-    assert.strictEqual(add(newGraph, "a", "b"), true);
+    const graph = new Graph();
+    graph.add("a", "b");
+    const newGraph = graph.from("c");
+    assert.strictEqual(newGraph.has("a", "b"), false);
   });
   test("1/3", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "c");
-    const newGraph = from(graph, "a");
-    assert.strictEqual(add(newGraph, "b", "c"), false);
-    assert.strictEqual(add(newGraph, "a", "b"), false);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "c");
+    const newGraph = graph.from("a");
+    assert.strictEqual(newGraph.has("b", "c"), true);
+    assert.strictEqual(newGraph.has("a", "b"), true);
   });
   test("2/3", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "c");
-    const newGraph = from(graph, "b");
-    assert.strictEqual(add(newGraph, "b", "c"), false);
-    assert.strictEqual(add(newGraph, "a", "b"), true);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "c");
+    const newGraph = graph.from("b");
+    assert.strictEqual(newGraph.has("b", "c"), true);
+    assert.strictEqual(newGraph.has("a", "b"), false);
   });
   test("multi inputs", () => {
-    const graph = create();
-    add(graph, "a", "c");
-    add(graph, "b", "c");
-    const newGraph = from(graph, "b");
-    assert.strictEqual(add(newGraph, "a", "c"), true);
-    assert.strictEqual(add(newGraph, "b", "c"), false);
+    const graph = new Graph();
+    graph.add("a", "c");
+    graph.add("b", "c");
+    const newGraph = graph.from("b");
+    assert.strictEqual(newGraph.has("a", "c"), false);
+    assert.strictEqual(newGraph.has("b", "c"), true);
   });
   test("multi inputs, from both", () => {
-    const graph = create();
-    add(graph, "a", "c");
-    add(graph, "b", "c");
-    const newGraph = from(graph, "a", "b");
-    assert.strictEqual(add(newGraph, "a", "c"), false);
-    assert.strictEqual(add(newGraph, "b", "c"), false);
+    const graph = new Graph();
+    graph.add("a", "c");
+    graph.add("b", "c");
+    const newGraph = graph.from("a", "b");
+    assert.strictEqual(newGraph.has("a", "c"), true);
+    assert.strictEqual(newGraph.has("b", "c"), true);
   });
   test("multi outputs", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    const newGraph = from(graph, "a");
-    assert.strictEqual(add(newGraph, "a", "b"), false);
-    assert.strictEqual(add(newGraph, "a", "c"), false);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("a", "c");
+    const newGraph = graph.from("a");
+    assert.strictEqual(newGraph.has("a", "b"), true);
+    assert.strictEqual(newGraph.has("a", "c"), true);
   });
 });
 
 describe("to", () => {
   test("1/2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    const newGraph = to(graph, "a");
-    assert.strictEqual(add(newGraph, "a", "b"), true);
+    const graph = new Graph();
+    graph.add("a", "b");
+    const newGraph = graph.to("a");
+    assert.strictEqual(newGraph.has("a", "b"), false);
   });
   test("2/2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    const newGraph = to(graph, "b");
-    assert.strictEqual(add(newGraph, "a", "b"), false);
+    const graph = new Graph();
+    graph.add("a", "b");
+    const newGraph = graph.to("b");
+    assert.strictEqual(newGraph.has("a", "b"), true);
   });
   test("?/2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    const newGraph = to(graph, "c");
-    assert.strictEqual(add(newGraph, "a", "b"), true);
+    const graph = new Graph();
+    graph.add("a", "b");
+    const newGraph = graph.to("c");
+    assert.strictEqual(newGraph.has("a", "b"), false);
   });
   test("2/3", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "c");
-    const newGraph = to(graph, "b");
-    assert.strictEqual(add(newGraph, "a", "b"), false);
-    assert.strictEqual(add(newGraph, "b", "c"), true);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "c");
+    const newGraph = graph.to("b");
+    assert.strictEqual(newGraph.has("a", "b"), true);
+    assert.strictEqual(newGraph.has("b", "c"), false);
   });
   test("3/3", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "c");
-    const newGraph = to(graph, "c");
-    assert.strictEqual(add(newGraph, "b", "c"), false);
-    assert.strictEqual(add(newGraph, "a", "b"), false);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "c");
+    const newGraph = graph.to("c");
+    assert.strictEqual(newGraph.has("b", "c"), true);
+    assert.strictEqual(newGraph.has("a", "b"), true);
   });
   test("multi inputs", () => {
-    const graph = create();
-    add(graph, "a", "c");
-    add(graph, "b", "c");
-    const newGraph = to(graph, "c");
-    assert.strictEqual(add(newGraph, "a", "c"), false);
-    assert.strictEqual(add(newGraph, "b", "c"), false);
+    const graph = new Graph();
+    graph.add("a", "c");
+    graph.add("b", "c");
+    const newGraph = graph.to("c");
+    assert.strictEqual(newGraph.has("a", "c"), true);
+    assert.strictEqual(newGraph.has("b", "c"), true);
   });
   test("multi outputs", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    const newGraph = to(graph, "b");
-    assert.strictEqual(add(newGraph, "a", "b"), false);
-    assert.strictEqual(add(newGraph, "a", "c"), true);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("a", "c");
+    const newGraph = graph.to("b");
+    assert.strictEqual(newGraph.has("a", "b"), true);
+    assert.strictEqual(newGraph.has("a", "c"), false);
   });
   test("multi outputs, to both", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    const newGraph = to(graph, "b", "c");
-    assert.strictEqual(add(newGraph, "a", "b"), false);
-    assert.strictEqual(add(newGraph, "a", "c"), false);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("a", "c");
+    const newGraph = graph.to("b", "c");
+    assert.strictEqual(newGraph.has("a", "b"), true);
+    assert.strictEqual(newGraph.has("a", "c"), true);
   });
 });
 
 describe("getRows", () => {
   test("empty", () => {
-    const graph = create();
-    assert.deepStrictEqual(getRows(graph), []);
+    const graph = new Graph();
+    assert.deepStrictEqual(graph.getRows(), []);
   });
   test("single", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    assert.deepStrictEqual(getRows(graph), ["a", "b"]);
+    const graph = new Graph();
+    graph.add("a", "b");
+    assert.deepStrictEqual(graph.getRows(), ["a", "b"]);
   });
   test("multi inputs", () => {
-    const graph = create();
-    add(graph, "a", "c");
-    add(graph, "b", "c");
-    assert.deepStrictEqual(getRows(graph), ["a", "b", "c"]);
+    const graph = new Graph();
+    graph.add("a", "c");
+    graph.add("b", "c");
+    assert.deepStrictEqual(graph.getRows(), ["a", "b", "c"]);
   });
   test("multi outputs", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    assert.deepStrictEqual(getRows(graph), ["a", "b", "c"]);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("a", "c");
+    assert.deepStrictEqual(graph.getRows(), ["a", "b", "c"]);
   });
   test("triangle", () => {
-    const graph = create();
-    add(graph, "b", "c");
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    assert.deepStrictEqual(getRows(graph), ["a", "b", "c"]);
+    const graph = new Graph();
+    graph.add("b", "c");
+    graph.add("a", "b");
+    graph.add("a", "c");
+    assert.deepStrictEqual(graph.getRows(), ["a", "b", "c"]);
   });
   test("diamond", () => {
-    const graph = create();
-    add(graph, "b", "d");
-    add(graph, "c", "d");
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    assert.deepStrictEqual(getRows(graph), ["a", "b", "c", "d"]);
+    const graph = new Graph();
+    graph.add("b", "d");
+    graph.add("c", "d");
+    graph.add("a", "b");
+    graph.add("a", "c");
+    assert.deepStrictEqual(graph.getRows(), ["a", "b", "c", "d"]);
   });
   test("circular 1", () => {
-    const graph = create();
-    add(graph, "a", "a");
-    assert.throws(() => getRows(graph));
+    const graph = new Graph();
+    graph.add("a", "a");
+    assert.throws(() => graph.getRows());
   });
   test("circular 2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "a");
-    assert.throws(() => getRows(graph));
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "a");
+    assert.throws(() => graph.getRows());
   });
   test("circular 3", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "c");
-    add(graph, "c", "a");
-    assert.throws(() => getRows(graph));
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "c");
+    graph.add("c", "a");
+    assert.throws(() => graph.getRows());
   });
 });
 
 describe("getCols", () => {
   test("empty", () => {
-    const graph = create();
-    assert.deepStrictEqual(getCols(graph), []);
+    const graph = new Graph();
+    assert.deepStrictEqual(graph.getCols(), []);
   });
   test("single", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    assert.deepStrictEqual(getCols(graph), [["a"], ["b"]]);
+    const graph = new Graph();
+    graph.add("a", "b");
+    assert.deepStrictEqual(graph.getCols(), [["a"], ["b"]]);
   });
   test("multi inputs", () => {
-    const graph = create();
-    add(graph, "a", "c");
-    add(graph, "b", "c");
-    assert.deepStrictEqual(getCols(graph), [["b"], ["a"], ["c"]]);
+    const graph = new Graph();
+    graph.add("a", "c");
+    graph.add("b", "c");
+    assert.deepStrictEqual(graph.getCols(), [["b"], ["a"], ["c"]]);
   });
   test("multi outputs", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    assert.deepStrictEqual(getCols(graph), [["a"], ["c", "b"]]);
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("a", "c");
+    assert.deepStrictEqual(graph.getCols(), [["a"], ["c", "b"]]);
   });
   test("triangle", () => {
-    const graph = create();
-    add(graph, "b", "c");
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    assert.deepStrictEqual(getCols(graph), [["a"], ["b"], ["c"]]);
+    const graph = new Graph();
+    graph.add("b", "c");
+    graph.add("a", "b");
+    graph.add("a", "c");
+    assert.deepStrictEqual(graph.getCols(), [["a"], ["b"], ["c"]]);
   });
   test("diamond", () => {
-    const graph = create();
-    add(graph, "b", "d");
-    add(graph, "c", "d");
-    add(graph, "a", "b");
-    add(graph, "a", "c");
-    assert.deepStrictEqual(getCols(graph), [["a"], ["c"], ["b"], ["d"]]);
+    const graph = new Graph();
+    graph.add("b", "d");
+    graph.add("c", "d");
+    graph.add("a", "b");
+    graph.add("a", "c");
+    assert.deepStrictEqual(graph.getCols(), [["a"], ["c"], ["b"], ["d"]]);
   });
   test("hexagon", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "c");
-    add(graph, "c", "d");
-    add(graph, "a", "e");
-    add(graph, "e", "f");
-    add(graph, "f", "d");
-    assert.deepStrictEqual(getCols(graph), [
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "c");
+    graph.add("c", "d");
+    graph.add("a", "e");
+    graph.add("e", "f");
+    graph.add("f", "d");
+    assert.deepStrictEqual(graph.getCols(), [
       ["a"],
       ["e"],
       ["f", "b"],
@@ -244,52 +243,48 @@ describe("getCols", () => {
     ]);
   });
   test("circular 1", () => {
-    const graph = create();
-    add(graph, "a", "a");
-    assert.doesNotThrow(() => getCols(graph));
+    const graph = new Graph();
+    graph.add("a", "a");
+    assert.doesNotThrow(() => graph.getCols());
   });
   test("circular 2", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "a");
-    assert.doesNotThrow(() => getCols(graph));
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "a");
+    assert.doesNotThrow(() => graph.getCols());
   });
   test("circular 3", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "c");
-    add(graph, "c", "a");
-    assert.doesNotThrow(() => getCols(graph));
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "c");
+    graph.add("c", "a");
+    assert.doesNotThrow(() => graph.getCols());
   });
 });
 
 describe("getLines", () => {
   test("empty", () => {
-    const graph = create();
-    const rows = getRows(graph);
-    const cols = getCols(graph);
-    assert.deepStrictEqual([...getLines(graph, rows, cols)], []);
+    const graph = new Graph();
+    assert.deepStrictEqual([...graph.getLines()], []);
   });
   test("complex (preview)", () => {
-    const graph = create();
-    add(graph, "a", "b");
-    add(graph, "b", "d");
-    add(graph, "b", "c");
-    add(graph, "b", "h");
-    add(graph, "c", "d");
-    add(graph, "d", "e");
-    add(graph, "f", "g");
-    add(graph, "g", "h");
-    add(graph, "h", "e");
-    add(graph, "h", "i");
-    add(graph, "i", "j");
-    add(graph, "j", "e");
-    add(graph, "k", "g");
-    add(graph, "k", "i");
-    add(graph, "k", "h");
-    const rows = getRows(graph);
-    const cols = getCols(graph);
-    for (const line of getLines(graph, rows, cols)) {
+    const graph = new Graph();
+    graph.add("a", "b");
+    graph.add("b", "d");
+    graph.add("b", "c");
+    graph.add("b", "h");
+    graph.add("c", "d");
+    graph.add("d", "e");
+    graph.add("f", "g");
+    graph.add("g", "h");
+    graph.add("h", "e");
+    graph.add("h", "i");
+    graph.add("i", "j");
+    graph.add("j", "e");
+    graph.add("k", "g");
+    graph.add("k", "i");
+    graph.add("k", "h");
+    for (const line of graph.getLines()) {
       console.log(line);
     }
   });
